65b9f698d9567b1dbf88b8b33c7b393a1dbcd66c
Quinn Elder <qre.turing@gmail.com>
2021-12-10 15:58:10 -0500

    Continu TDD for Song List

--- redux-1/src/Components/SongList/SongList.test.tsx
+++ redux-1/src/Components/SongList/SongList.test.tsx
@@ -57,41 +57,33 @@ describe("Song List Testing Suite", () => {
 
   })
 
-  describe("Song List Default Elements", () => {
-    it("Should render a Song Container", () => {
+  describe("Song List Rendered Values", () => {
+    it("Should render 4 Song Names", () => {
+      const { getAllByTestId } = render(router);
+
+      const Buttons = getAllByTestId("Song-Name")
+      expect(Buttons.map(button => {
+        switch (button.textContent) {
+          case "No Scrubs": return true
+          case "Ride The Lightning": return true
+          case "Ready Or Not!": return true
+          case "I Want it That Way": return true
+          default: return false
+        }
+      }).filter(bool => {
+        if (bool === true) return bool
+      })).toHaveLength(4);
+    });
+  })
+
+  describe("Song List Events", () => {
+    it("Should be able to select a Song", () => {
       const { getByTestId } = render(router);
 
       const song_container = getByTestId("Songs-Container");
       expect(song_container).toBeInTheDocument();
-    });
 
-    it("Should render 4 Song Wrappers", () => {
-      const { getAllByTestId } = render(router);
-
-      const Song_Wrapper = getAllByTestId("Song-Wrapper");
-      expect(Song_Wrapper).toHaveLength(4);
-    });
-
-    it("Should render 4 Button Wrappers", () => {
-      const { getAllByTestId } = render(router);
-
-      const Button_Wrapper = getAllByTestId("Button-Wrapper");
-      expect(Button_Wrapper).toHaveLength(4);
-    });
-
-
-    it("Should render 4 Buttons", () => {
-      const { getAllByRole } = render(router);
-
-      const Buttons = getAllByRole("button", { name: "Select" });
-      expect(Buttons).toHaveLength(4);
-    });
-
-    it("Should render 4 Song Names", () => {
-      const { getAllByTestId } = render(router);
-
-      const Buttons = getAllByTestId("Song-Name");
-      expect(Buttons).toHaveLength(4);
     });
   })
+
 })
