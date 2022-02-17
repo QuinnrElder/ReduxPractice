b5d6c7a141ce022985abe331256827f1a001e581
Quinn Elder <quinn@heysparkbox.com>
2022-02-17 11:53:05 -0500

    :art: Feat: Add theme to Reducer in store

--- redux-with-toolkit/src/index.js
+++ redux-with-toolkit/src/index.js
@@ -8,11 +8,13 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
 import userReducer from "./Features/user";
+import themeReducer from "./Features/theme"
 
 //VARIABLES
 const store = configureStore({
   reducer: {
     user: userReducer,
+    theme: themeReducer,
   },
 })
 
