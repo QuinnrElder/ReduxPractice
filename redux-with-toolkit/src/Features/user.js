93d8fb08fc650744521253d441a9e4964bb92956
Quinn Elder <quinn@heysparkbox.com>
2022-02-17 11:54:22 -0500

    :art: Refactor: Update variables

--- redux-with-toolkit/src/Features/user.js
+++ redux-with-toolkit/src/Features/user.js
@@ -1,13 +1,18 @@
 import {createSlice} from '@reduxjs/toolkit'
 
+const defaultUser = {name: "",  age: 0,  email:""}
+
 export const userSlice = createSlice({
   name: "user",
-  initialState: {value: {name: "",  age: 0,  email:""}},
+  initialState: {value: defaultUser},
   reducers: {
     login: (state, action) => {
         state.value = action.payload;
+    },
+    logout: (state) => {
+      state.value = defaultUser
     }
   }
 })
-
+export const {login, logout} = userSlice.actions
 export default  userSlice.reducer;
