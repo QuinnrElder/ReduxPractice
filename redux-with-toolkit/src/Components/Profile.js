1e462dee165fdfee70e4f21b9e5aeb84016da3db
Quinn Elder <quinn@heysparkbox.com>
2022-02-17 10:34:43 -0500

    :art: Feat: Create basic components

---
+++ redux-with-toolkit/src/Components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Name: </p>
+      <p>Age:</p>
+      <p>Email:</p>
+    </div>
+  )
+}
+  
