522316a33b4593f9358df31c45c242705b1be709
Quinn Elder <quinn@heysparkbox.com>
2022-02-17 10:53:58 -0500

    :art: Feat: Begin creating redux reducer

--- redux-with-toolkit/src/index.js
+++ redux-with-toolkit/src/index.js
@@ -4,9 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//REDUX
+import { Provider } from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import userReducer from "./Features/user";
+
+//VARIABLES
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
