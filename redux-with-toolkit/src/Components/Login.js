37cd8630f7402e645e8bb1bce56d9acd9f66d86b
Quinn Elder <quinn@heysparkbox.com>
2022-02-17 11:53:38 -0500

    :art: Feat: Update Login

--- redux-with-toolkit/src/Components/Login.js
+++ redux-with-toolkit/src/Components/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../Features/user";
 
 export function Login() {
-  return (
-    <div>
-      <button>Login</button>
-    </div>
-  )
+	const dispatch = useDispatch();
+
+	return (
+		<div>
+			<button
+				onClick={() => {
+					dispatch(
+						login({ name: "Quinn", age: 27, email: "bbblblb@kgsksf.com" })
+					);
+				}}
+			>
+				Login
+			</button>
+			<button
+				onClick={() => {
+					dispatch(logout());
+				}}
+			>
+				Logout
+			</button>
+		</div>
+	);
 }
