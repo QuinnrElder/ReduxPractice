acf515c8d035305c7814843f942b9d09b95cef4a
Quinn Elder <quinn@heysparkbox.com>
2022-02-21 17:10:02 -0500

    :art: Refactor: Update init state

--- redux-udemy/src/Redux/initialState.js
+++ redux-udemy/src/Redux/initialState.js
@@ -1,6 +1,22 @@
 export const initialState = {
-  todos: [{
+  todos: [
+    {
     id: 0,
     title: "Buy Milk",
-  }],
+    completed: false,
+    important: false,
+  },
+    {
+    id: 1,
+    title: "Clean Room",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 2,
+    title: "Food Prep",
+    completed: true,
+    important: true,
+  },
+],
 }
