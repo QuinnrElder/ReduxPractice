83837c8b611daa2984a880477783dc9ca81fecbc
Quinn Elder <quinn@heysparkbox.com>
2022-02-22 09:50:11 -0500

    :art: Feat: Update the Redux workflow
    - Add new Actions
    - Add fn() to Reducer

--- redux-udemy/src/Redux/actions.js
+++ redux-udemy/src/Redux/actions.js
@@ -1,1 +1,4 @@
 export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED'
+export const DELETE_TODO = 'DELETE_TODO'
+export const FAVORITE_TODO = 'FAVORITE_TODO'
--- redux-udemy/src/Redux/reducer.js
+++ redux-udemy/src/Redux/reducer.js
@@ -1,7 +1,65 @@
-import {initialState} from './initialState'
+import { initialState } from "./initialState";
 
+//ACTIONS
+import { ADD_TODO, TOGGLE_COMPLETED, DELETE_TODO, FAVORITE_TODO } from "../Redux/actions";
+
+//ID GENERATOR
+import shortid from "shortid";
+
+//REDUCER
 function reducer(state = initialState, action) {
-  return state
+	switch (action.type) {
+		case ADD_TODO: {
+			const { title } = action.payload;
+			const newState = {
+				...state,
+				todos: [
+					...state.todos,
+					{ title: title, id: shortid(), completed: false, important: false },
+				],
+			};
+			return newState;
+		}
+		case TOGGLE_COMPLETED: {
+			const { id } = action.payload;
+			const newTodos = state.todos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			});
+			const newState = {
+				...state,
+				todos: newTodos,
+			};
+			return newState;
+		}
+		case DELETE_TODO: {
+			const { id } = action.payload;
+			const newTodos = state.todos.filter((todo) => todo.id !== id)
+			const newState = {
+				...state,
+				todos: newTodos,
+			};
+			return newState;
+		}
+		case FAVORITE_TODO: {
+			const { id } = action.payload;
+			const newTodos = state.todos.map((todo) => {
+				if(todo.id === id) {
+					todo.important = !todo.important;
+				}
+				return todo
+			})
+			const newState = {
+				...state,
+				todos: newTodos,
+			};
+			return newState;
+		}
+		default:
+			return state;
+	}
 }
 
 export default reducer;
